580c63bfe0e2be723b64abf2badd63cd173d8f44
Yayu Wang <st.saint.wyy@gmail.com>
2023-01-24 17:30:47 -0800

    update: add dwm_battery & remove icons

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" "	, "dwm_volume",          1,             0},
-	{" "	, "dwm_monitor",         3,		0},
-	{" "	, "dwm_clock",		 1,		0},
+        /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+        {"|"	, "dwm_volume",          1,             0},
+        {""     , "dwm_battery",         5,             0},
+        {""	, "dwm_monitor",         3,		0},
+        {""	, "dwm_clock",		 1,		0},
 
 };
 
